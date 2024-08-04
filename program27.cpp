7f98423b5ffa9193fcc2c648b7e0e55b18041dde
AbhijitDohale <165358776+AbhijitDohale@users.noreply.github.com>
2024-08-04 22:51:16 +0530

    Add files via upload

---
+++ program27.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n;
+cout<<"enter number:"<<endl;
+cin>>n;
+int row = 1;
+ char ch = 'A';
+while(row<=n){
+    int column = 1;
+   
+    while(column<=n){
+        cout <<ch<<" ";
+        ch = ch + 1;
+        column = column + 1;
+    }
+cout <<endl;
+row = row + 1;
+
+}
+
+
+return 0;
+}
